03b5ff0cdd68e03e1be6fd5c78b543f4dbddfe6f
Mikhail Astashonak <thenaitrise@gmail.com>
2020-10-08 18:02:18 +0300

    comments moved

--- main.cpp
+++ main.cpp
@@ -1,51 +1,34 @@
-// Задание: Ввод с клавиатуры натурального числа и вывод в консоль 
-// всех простых чисел не превышающих исходное натуральное число
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 int main() {
-  // Ввод n с клавиатуры проверка на "натуральность"
   int n;
-  std::cout << "Enter int in N" << endl;
-  std::cin >> n;
+  cout << "Enter int in N" << endl;
+  cin >> n;                                   // Ввод n с клавиатуры 
 
-  // Проверка на "натуральность"
-  if (n <= 0) {
-    // Завершаю выполнение функции main
-    std::cout << "Invalid num";
-    return 0;
+  if (n <= 0) {                               // Проверка на "натуральность"
+    cout << "Invalid num";                    
+    return 0;                                 // Завершаю выполнение функции main
   }
 
-  // i - для перебора всех чисел и проверки их на "простоту"
-  int i;
+  int currentDozens = 0;                      // currentDozens - для контроля десятков
 
-  // currentDecade - для контролятекущего разряда десятков
-  int currentDecade = 0;
+  for (int i = 2; i <= n; i++) {              // Цикл 1 уровня => перебор всех чисел от 1 до n
+    if ((i % 2) == 0 && i != 2) continue;     // Проверка на чётность
 
-  // Цикл 1 уровня => перебор всех чисел от 1 до n
-  for (i = 2; i <= n; i++) 
-  {
-    // Проверка на чётность
-    if ((i % 2) == 0 && i != 2) continue;
+    bool isPrime = true;                      // являетсяЛиПростым
 
-    // являетсяЛиПростым
-    bool isPrime = true;
-
-    // Перебор делителей
-    for (int j = 3 ; j <= sqrt(i); j += 2)
-    {
-      // "Ловля" составного числа 
-      if ((i % j) == 0) isPrime = false;
+    for (int j = 3 ; j <= sqrt(i); j += 2) {  // Перебор делителей
+      if ((i % j) == 0) isPrime = false;      // "Ловля" составного числа 
     }
 
     if (isPrime)  {
-      // Если число десятков изменится, переход на следующую строку 
-      if (currentDecade != floor(i/10)) {
-        std::cout << endl;
-        currentDecade++; 
+      if (currentDozens != floor(i/10)) {
+        cout << endl;                         // Если число десятков изменится,  
+        currentDozens++;                      // переход на следующую строку
       }
-      std::cout << i << " ";
+      cout << i << " ";
     }
   }  
 }
