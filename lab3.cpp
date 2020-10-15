8b59c2a423ac9cfea73c314d5309b47fa52bebd9
Mikhail Astashonak <thenaitrise@gmail.com>
2020-10-15 16:11:34 +0300

    lab

--- lab3.cpp
+++ lab3.cpp
@@ -1,20 +1,22 @@
+// Лабораторная 3
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 int main() {
-  int number;
-  cin >> number;
+  int n;
+  cin >> n;
     
   int dozensAmount = 0;
-  for (int i = 2; i <= number; i++) {
+  for (int i = 2; i <= n; i++) {
 
     if ((i % 2) == 0 && i != 2) {
       continue;
     }
 
     bool isPrime = true;
-    for (int j = 3; j <= sqrt(i); j += 2) {
+    float c = sqrt(i);
+    for (int j = 3; j <= c; j += 2) {
       if ((i % j) == 0) {
         isPrime = false;
         break;
@@ -24,7 +26,7 @@ int main() {
     if (isPrime) {
       if (dozensAmount < floor(i / 10)) {
         cout << endl;
-        dozensAmount++;
+        dozensAmount = floor(i / 10);
       } 
       cout << i << " ";
     }
