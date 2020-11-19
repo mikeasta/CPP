bbc63c392b0393adfac5bfbc114962850ef97caa
Mikhail Astashonak <thenaitrise@gmail.com>
2020-11-19 12:25:46 +0300

    lab5 fixed

--- lab5.cpp
+++ lab5.cpp
@@ -3,30 +3,44 @@ using namespace std;
 
 int main() 
 {
+    // arr - dinamic
+    // init - initial array
+    // buff - buffer array
+
     // 1. Read an array
-    unsigned long long arr[3][3], buff[3][3], a, b, c;
-    int n, i, j;
+    unsigned long long arr[3][3], init[3][3], buff[3];
+    int n, i, j, k;
     for (i = 0; i < 3; i++) 
     {
         for (j = 0; j < 3; j++)
         {
             cin >> arr[i][j];
-            buff[i][j] = arr[i][j];
+            init[i][j] = arr[i][j];
         }
     }
     cin >> n;
 
     // 2. Pow
+    // Перебор степеней
     for (; n > 1 ; n--)
     {
+        // Перебор строк
         for (i = 0; i < 3 ; i++)
         {
-            a = arr[i][0];
-            b = arr[i][1];
-            c = arr[i][2];
+            // Резервирование значений строки i
             for (j = 0; j < 3; j++)
             {
-                arr[i][j] = (buff[0][j] * a) + (buff[1][j] * b) + (buff[2][j] * c);
+                buff[j] = arr[i][j];
+            }
+
+            // Перебор каждого элемента столбца
+            for (j = 0; j < 3; j++)
+            {
+                arr[i][j] = 0;
+                for (k = 0; k < 3; k++)
+                {
+                    arr[i][j] += buff[k] * init[k][j];
+                }
             }
         }
     }
